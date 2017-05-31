90e25603fe297b6d79fef612132aadfbd3dacb50
Mareks <m.kalejs@outlook.com>
2017-05-31 12:56:14 -0700

    Updated version

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// New Code
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('mongodb://Kalpens:weaksecurity123@ds133231.mlab.com:33231/mareksmd');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
+var albums = require('./routes/albums');
 
 var app = express();
 
@@ -19,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,25 +33,38 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
+app.use('/albums', albums);
 
-// catch 404 and forward to error handler
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/// error handlers
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mareks favourtie albums' });
 });
-
+/* GET Album list page. */
+router.get('/albums', function(req, res) {
+    var db = req.db;
+    var collection = db.get('albumList');
+    collection.find({},{},function(e,docs){
+        res.render('albumList', {
+            "almbumList" : docs,
+            title: 'View favourite albums'
+        });
+    });
+});
+/* GET New Album page. */
+router.get('/newalbum', function(req, res) {
+    res.render('newAlbum', { title: 'Add New Album' });
+});
 module.exports = router;
